6a1173ef0212ed3966372e8da9df0bc3754ecd57
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2024-08-21 09:50:58 +0500

    Create Q6.py

---
+++ Labs/Lab1/Q6.py
@@ -0,0 +1,7 @@
+marks = {
+    "Physics": int(input("Enter marks for physics: ")),
+    "chemistry": int(input("Enter marks for chemistry")),
+    "Math": int(input("Enter marks for math"))
+}
+
+avgmarks = sum
