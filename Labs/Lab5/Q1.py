653d6a88c1b42d271b752256bb978811eedd8fa4
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2024-09-18 09:10:34 +0500

    Create Q1.py

---
+++ Labs/Lab5/Q1.py
@@ -0,0 +1,19 @@
+class Vehicle:
+    def __init__(self, seating_capacity):
+        self.seating_capacity = seating_capacity
+
+    def calculate_fare(self):
+        return self.seating_capacity * 100
+
+
+class Bus(Vehicle):
+    def calculate_fare(self): 
+        base_fare = super().calculate_fare()
+        maintenance_charge = base_fare * 0.10
+        total_fare = base_fare + maintenance_charge
+        return total_fare
+
+
+
+bus = Bus(seating_capacity=50)
+print(f"Total fare for the bus: {bus.calculate_fare()}")  
