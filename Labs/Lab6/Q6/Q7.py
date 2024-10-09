0c1bb99a557488b6882fef2978570a55e256a899
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2024-10-09 08:11:52 +0500

    Delete Labs/Lab6/Q6 directory

--- Labs/Lab6/Q6/Q7.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-file_path = 'employee.xlsx'  
-df = pd.read_excel(file_path)
-print("DataFrame is now Loaded:")
-print(df.head())  
-specified_year = 2023  
-employees_of_year = df[df['Year'] == specified_year]  
-print(f"\nEmployees of the year {specified_year}:")
-print(employees_of_year)
